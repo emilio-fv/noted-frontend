6ce8bfa8840e15e44a177b8d5a4b6cebe38efdd8
Emilio Vazquez <emiliofv.sd@gmail.com>
2024-01-08 21:10:57 -0700

    connect to protected route to store

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,7 +1,14 @@
+import { connect } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ isLoggedIn }) => {
     return isLoggedIn ? <Outlet /> : <Navigate to='/' />;
 }
 
-export default ProtectedRoute;
+const mapPropsToState = (state) => {
+    return {
+        isLoggedIn: state.auth.isLoggedIn,
+    }
+};
+
+export default connect(mapPropsToState)(ProtectedRoute);
