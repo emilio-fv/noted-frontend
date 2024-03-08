87dae79d0a596352f55812ba1d93d00b55d15f96
Emilio Vazquez <emiliofv.sd@gmail.com>
2024-03-08 00:14:07 -0700

    fix modal and reroute bugs upon successful login

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ isLoggedIn }) => {
-    return isLoggedIn ? <Outlet /> : <Navigate to='/' />;
+const ProtectedRoute = ({ loggedInUser }) => {
+    return loggedInUser ? <Outlet /> : <Navigate to='/' />;
 }
 
 export default ProtectedRoute;
